7e5c3ae3ce9d3d06832820b62b38872ea745cee3
Jim Thoburn <jim@jimthoburn.com>
2016-08-07 21:52:47 -0700

    Generate source pages using a plugin for Jekyll

---
+++ _plugins/source_generator.rb
@@ -0,0 +1,24 @@
+# KUDOS: https://github.com/bighairydave/jekyll-datalist/blob/master/_plugins/generator.rb
+module Jekyll
+  class SourceListGenerator < Generator
+    def generate(site)
+      site.data['food_banks_andrew'].each do | source |
+        site.pages << SourcePage.new(site, "/", "source/#{ source['name'].downcase.gsub(" ", "-").gsub("/", "-").gsub("&", "-") }", source)
+      end
+    end
+  end
+
+  class SourcePage < Page
+    def initialize(site, base, dir, source)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process('index.html')
+      self.read_yaml(File.join(base, '_layouts'), 'source.html')
+      self.data['title'] = source['name']
+      self.data['source'] = source
+    end
+  end
+
+end
