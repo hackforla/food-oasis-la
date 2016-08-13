a77e7ead24144c8b4d80840a5dc42a15dde97132
Jim Thoburn <jim@jimthoburn.com>
2016-08-12 19:18:36 -0700

    Make titles on the food source page look a little nicer.

--- _plugins/food_source_generator.rb
+++ _plugins/food_source_generator.rb
@@ -3,7 +3,13 @@ module Jekyll
   class SourceListGenerator < Generator
     def generate(site)
       site.data['food_banks_andrew'].each do | food_source |
-        food_source['uri'] = food_source['name'].downcase.gsub(" ", "-").gsub("/", "-").gsub("&", "-").gsub(".", "-").gsub("'", "").gsub("--", "-")
+        food_source['uri'] = food_source['name'].downcase
+                                                .gsub(" ", "-")
+                                                .gsub("/", "-")
+                                                .gsub("&", "-")
+                                                .gsub(".", "-")
+                                                .gsub("'", "")
+                                                .gsub("--", "-")
         site.pages << SourcePage.new(site, "/", "source/#{ food_source['uri'] }", food_source)
       end
     end
@@ -17,6 +23,20 @@ module Jekyll
       @name = 'index.html'
       self.process('index.html')
       self.read_yaml(File.join(base, '_layouts'), 'food-source.html')
+      name_html  = food_source['name']
+      name_words = food_source['name'].split(' ')
+
+      # If there are at least three words
+      if name_words.length >= 3
+
+        # Wrap the last two words in a span
+        name_words.insert(name_words.length - 2, '<span>')
+        name_words.push('</span>')
+
+        name_html = name_words.join(' ')
+      end
+
+      self.data['name_html'] = name_html.gsub('&', '&amp;') # TODO: Consider escaping < and > as well
       self.data['title'] = food_source['name']
       self.data['food_source'] = food_source
     end
