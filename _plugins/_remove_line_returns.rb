a0fb2655037aad4974f98be7e1be5b94d98031d5
Jim Thoburn <jim@jimthoburn.com>
2016-08-24 21:27:34 -0700

    Amend my last commit.

--- _plugins/_remove_line_returns.rb
+++ _plugins/_remove_line_returns.rb
@@ -2,7 +2,7 @@ module Jekyll
   module RemoveLineReturns
     def remove_line_returns(input)
     	# KUDOS: http://stackoverflow.com/questions/287713/how-do-i-remove-carriage-returns-with-ruby#answer-287810
-    	input.gsub(/\r\n?/, '')
+    	input.gsub(/\r\n?/, ', ')
     end
   end
 end
