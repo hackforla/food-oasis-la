da0d77bbb758192ee8722be5e52ad9eda62add45
Jim Thoburn <jim@jimthoburn.com>
2016-08-24 21:29:06 -0700

    Aand… amend my last commit.

--- _plugins/_remove_line_returns.rb
+++ _plugins/_remove_line_returns.rb
@@ -1,10 +1,10 @@
 module Jekyll
-  module RemoveLineReturns
-    def remove_line_returns(input)
+  module LineReturnsToCommas
+    def line_returns_to_commas(input)
     	# KUDOS: http://stackoverflow.com/questions/287713/how-do-i-remove-carriage-returns-with-ruby#answer-287810
     	input.gsub(/\r\n?/, ', ')
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::RemoveLineReturns)
+Liquid::Template.register_filter(Jekyll::LineReturnsToCommas)
