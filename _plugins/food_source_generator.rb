b7b2877887d4b78a1f0a2236249f4f2218aafa7d
Jim Thoburn <jim@jimthoburn.com>
2016-09-02 14:46:38 -0700

    Add grocery stores and supermarkets.

--- _plugins/food_source_generator.rb
+++ _plugins/food_source_generator.rb
@@ -1,24 +1,37 @@
 # KUDOS: https://github.com/bighairydave/jekyll-datalist/blob/master/_plugins/generator.rb
 module Jekyll
   class SourceListGenerator < Generator
+    def get_uri(name)
+      return name.downcase
+        .gsub(" ", "-")
+        .gsub("/", "-")
+        .gsub("&", "-")
+        .gsub(".", "-")
+        .gsub("'", "")
+        .gsub("--", "-")
+    end
     def generate_one(site, data_name, type, type_uri)
       site.data[data_name].each do | food_source |
         food_source['type']     = type
         food_source['type_uri'] = type_uri
-        food_source['uri'] = food_source['name'].downcase
-                                                .gsub(" ", "-")
-                                                .gsub("/", "-")
-                                                .gsub("&", "-")
-                                                .gsub(".", "-")
-                                                .gsub("'", "")
-                                                .gsub("--", "-")
+        food_source['uri'] = get_uri(food_source['name'])
+
+        # Food sources (particularly super markets) sometimes share a name with each other, so add the address to make a unique URI
+        if food_source['address'] != '' && !food_source['address'].nil?
+          food_source['uri'] = "#{ food_source['uri'] }/#{ get_uri(food_source['address']) }"
+        end
+
         site.pages << SourcePage.new(site, "/", "#{ type_uri }/#{ food_source['uri'] }", food_source)
       end
     end
     def generate(site)
-      generate_one(site, 'food_banks_andrew',        'Food Bank',        'food-bank')
-      generate_one(site, 'community_gardens_andrew', 'Community Garden', 'community-garden')
-      generate_one(site, 'farmers_markets_andrew',   'Farmers Market',   'farmers-market')
+      # These take a long time to build, so they’re commented out until we need them.
+      # 
+      # generate_one(site, 'food_banks_andrew',        'Food Bank',        'food-bank')
+      # generate_one(site, 'community_gardens_andrew', 'Community Garden', 'community-garden')
+      # generate_one(site, 'farmers_markets_andrew',   'Farmers Market',   'farmers-market')
+      # generate_one(site, 'grocery_08-30',            'Grocery Store',    'grocery-store')
+      # generate_one(site, 'supermarkets_08-30',       'Supermarket',      'supermarket')
     end
   end
 
