99ceb1fae14ea35ddfc5225ba6494539539f2e7e
Jim Thoburn <jim@jimthoburn.com>
2016-09-02 21:08:30 -0700

    Use latitude/longitude for detail page URIs

--- _plugins/food_source_generator.rb
+++ _plugins/food_source_generator.rb
@@ -1,33 +1,45 @@
 # KUDOS: https://github.com/bighairydave/jekyll-datalist/blob/master/_plugins/generator.rb
 module Jekyll
   class SourceListGenerator < Generator
-    def get_uri(name)
-      return name.downcase
-        .gsub(" ", "-")
+
+    # This is duplicated in map/index.html
+    def string_to_uri(string)
+      return string.downcase
+        .gsub("\s", "-")
         .gsub("/", "-")
         .gsub("&", "-")
         .gsub(".", "-")
+        .gsub(",", "-")
+        .gsub("+", "-")
+        .gsub(/\r\n?/, '-')
         .gsub("'", "")
+        .gsub("(", "")
+        .gsub(")", "")
+        .gsub("----", "-")
+        .gsub("---", "-")
         .gsub("--", "-")
+    end
+    def get_uri(food_source)
+      # Food sources (particularly super markets) sometimes share a name with each other, so add the latitude and longitude to make a unique URI.
+      uri = "#{ string_to_uri(food_source['name'])}/#{ food_source['latitude'] }/#{ food_source['longitude'] }"
+
+      # if food_source['address'] != '' && !food_source['address'].nil?
+      #   uri = "#{ uri }/#{ string_to_uri(food_source['address']) }"
+      # end
     end
     def generate_one(site, data_name, type, type_uri)
       site.data[data_name].each do | food_source |
         food_source['type']     = type
         food_source['type_uri'] = type_uri
-        food_source['uri'] = get_uri(food_source['name'])
-
-        # Food sources (particularly super markets) sometimes share a name with each other, so add the address to make a unique URI
-        if food_source['address'] != '' && !food_source['address'].nil?
-          food_source['uri'] = "#{ food_source['uri'] }/#{ get_uri(food_source['address']) }"
-        end
+        food_source['uri'] = get_uri(food_source)
 
         site.pages << SourcePage.new(site, "/", "#{ type_uri }/#{ food_source['uri'] }", food_source)
       end
     end
     def generate(site)
       generate_one(site, 'food_banks_andrew',        'Food Bank',        'food-bank')
-      generate_one(site, 'community_gardens_andrew', 'Community Garden', 'community-garden')
-      generate_one(site, 'farmers_markets_andrew',   'Farmers Market',   'farmers-market')
+      generate_one(site, 'community-gardens_08-30',  'Community Garden', 'community-garden')
+      generate_one(site, 'farmers_markets_master',   'Farmers Market',   'farmers-market')
       generate_one(site, 'grocery_08-30',            'Grocery Store',    'grocery-store')
       generate_one(site, 'supermarkets_08-30',       'Supermarket',      'supermarket')
     end
