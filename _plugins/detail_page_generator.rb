ce475ed6f4cbbccf7df13f08aa2d5fac57ada597
Jim Thoburn <jim@jimthoburn.com>
2016-10-14 09:36:04 -0700

    Rename food source detail page plugin.

--- _plugins/detail_page_generator.rb
+++ _plugins/detail_page_generator.rb
@@ -1,6 +1,34 @@
 # KUDOS: https://github.com/bighairydave/jekyll-datalist/blob/master/_plugins/generator.rb
 module Jekyll
-  class SourceListGenerator < Generator
+
+  class FoodSourceDetailPage < Page
+    def initialize(site, base, dir, food_source)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process('index.html')
+      self.read_yaml(File.join(base, '_layouts'), 'food-source.html')
+      name_html  = food_source['name']
+      name_words = food_source['name'].split(' ')
+
+      # If there are at least three words
+      if name_words.length >= 3
+
+        # Wrap the last two words in a span (so they can be styled to stay together on one line)
+        name_words.insert(name_words.length - 2, '<span>')
+        name_words.push('</span>')
+
+        name_html = name_words.join(' ')
+      end
+
+      self.data['name_html'] = name_html.gsub('&', '&amp;') # TODO: Consider escaping < and > as well
+      self.data['title'] = food_source['name']
+      self.data['food_source'] = food_source
+    end
+  end
+
+  class FoodSourceDetailGenerator < Generator
 
     # This is duplicated in map/index.html
     def string_to_uri(string)
@@ -23,10 +51,6 @@ module Jekyll
     def get_uri(food_source)
       # Food sources (particularly super markets) sometimes share a name with each other, so add the latitude and longitude to make a unique URI.
       uri = "#{ string_to_uri(food_source['name'])}/#{ food_source['latitude'] }/#{ food_source['longitude'] }"
-
-      # if food_source['address'] != '' && !food_source['address'].nil?
-      #   uri = "#{ uri }/#{ string_to_uri(food_source['address']) }"
-      # end
     end
     def generate_one(site, data_name, type, type_uri)
       site.data[data_name].each do | food_source |
@@ -34,7 +58,7 @@ module Jekyll
         food_source['type_uri'] = type_uri
         food_source['uri'] = get_uri(food_source)
 
-        site.pages << SourcePage.new(site, "/", "#{ type_uri }/#{ food_source['uri'] }", food_source)
+        site.pages << FoodSourceDetailPage.new(site, "/", "#{ type_uri }/#{ food_source['uri'] }", food_source)
       end
     end
     def generate(site)
@@ -46,33 +70,6 @@ module Jekyll
         generate_one(site, 'grocery-stores',    'Grocery Store',    'grocery-store')
         generate_one(site, 'grocery-other',     'Store',            'store')
       end
-    end
-  end
-
-  class SourcePage < Page
-    def initialize(site, base, dir, food_source)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
-      self.process('index.html')
-      self.read_yaml(File.join(base, '_layouts'), 'food-source.html')
-      name_html  = food_source['name']
-      name_words = food_source['name'].split(' ')
-
-      # If there are at least three words
-      if name_words.length >= 3
-
-        # Wrap the last two words in a span
-        name_words.insert(name_words.length - 2, '<span>')
-        name_words.push('</span>')
-
-        name_html = name_words.join(' ')
-      end
-
-      self.data['name_html'] = name_html.gsub('&', '&amp;') # TODO: Consider escaping < and > as well
-      self.data['title'] = food_source['name']
-      self.data['food_source'] = food_source
     end
   end
 
