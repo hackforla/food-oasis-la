1538f622a1f6ea78e6afa4edbdfc18b62d43b709
Jim Thoburn <jim@jimthoburn.com>
2017-05-30 19:17:57 -0700

    Generate locations from markdown files.

--- api/locations.js
+++ api/locations.js
@@ -3,7 +3,20 @@ layout: null
 ---
 var locations = [
 
-{% assign data_list = site.data['generated-locations-for-jekyll'] %}
+{% assign data_collection = site.collections | where: "label", "food-pantry" | first %}
+{% assign data_list = data_collection.docs %}
+{% include api-list.html %}
+,
+{% assign data_collection = site.collections | where: "label", "farmers-market" | first %}
+{% assign data_list = data_collection.docs %}
+{% include api-list.html %}
+,
+{% assign data_collection = site.collections | where: "label", "community-garden" | first %}
+{% assign data_list = data_collection.docs %}
+{% include api-list.html %}
+,
+{% assign data_collection = site.collections | where: "label", "supermarket" | first %}
+{% assign data_list = data_collection.docs %}
 {% include api-list.html %}
 
 ];
