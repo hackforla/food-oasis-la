be608ddb364fe4cfcf825f46f22ebdd8b4864b7c
Jim Thoburn <jim@jimthoburn.com>
2017-06-09 11:21:15 -0700

    Make the map page load more smoothly.

--- assets/js/location-list-toggle-view.js
+++ assets/js/location-list-toggle-view.js
@@ -1,41 +1,57 @@
+window.oasis = window.oasis || {};
 
 // Toggle between the map and list views
 (function() {
-	if (mapboxgl.supported()) {
 
-		var views = document.getElementById('search-views');
-		var template = document.getElementById('search-views-template');
-		views.insertAdjacentHTML('beforeend', template.innerHTML);
+	var views = document.getElementById('search-views');
+	var template = document.getElementById('search-views-template');
+	views.insertAdjacentHTML('beforeend', template.innerHTML);
 
-		var mapButton = document.getElementById('map-button');
-		var listButton = document.getElementById('list-button');
+	var mapButton = document.getElementById('map-button');
+	var listButton = document.getElementById('list-button');
 
+	listButton.style.display = "block";
+
+	document.body.classList.add('only-map');
+	document.body.classList.remove('only-list');
+
+	function showMap() {
+		mapButton.style.display = "none";
 		listButton.style.display = "block";
 
 		document.body.classList.add('only-map');
 		document.body.classList.remove('only-list');
+	}
 
-		mapButton.addEventListener('click', function(e) {
-			mapButton.style.display = "none";
-			listButton.style.display = "block";
+	function showList() {
+		mapButton.style.display = "block";
+		listButton.style.display = "none";
 
-			document.body.classList.add('only-map');
-			document.body.classList.remove('only-list');
-
-			e.preventDefault();
-		}, false);
-
-		listButton.addEventListener('click', function(e) {
-			mapButton.style.display = "block";
-			listButton.style.display = "none";
-
-			document.body.classList.remove('only-map');
-			document.body.classList.add('only-list');
-
-			e.preventDefault();
-		}, false);
-	} else {
-		if (console && console.log) console.log('MapboxGL doesn’t appear to be supported in this web browser. Showing the list instead…');
+		document.body.classList.remove('only-map');
 		document.body.classList.add('only-list');
 	}
+
+	function onMapClick(e) {
+		showMap();
+		e.preventDefault();
+	}
+
+	function onListClick(e) {
+		showList();
+		e.preventDefault();
+	}
+
+	function hideToggleButtons() {
+		mapButton.removeEventListener('click', onMapClick, false);
+		listButton.removeEventListener('click', onListClick, false);
+		mapButton.style.opacity = '0';
+		listButton.style.opacity = '0';
+	}
+
+	mapButton.addEventListener('click', onMapClick, false);
+	listButton.addEventListener('click', onListClick, false);
+
+	window.oasis.showMap  = showMap;
+	window.oasis.showList = showList;
+	window.oasis.hideToggleButtons = hideToggleButtons;
 })();
