ce737f9875d8cc94803dffab9335b4aa50d0279b
Jim Thoburn <jim@jimthoburn.com>
2017-11-28 21:32:06 -0800

    Add new user interface for filters

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -112,30 +112,43 @@ window.oasis = window.oasis || {};
 			let minutes = Number(timeString.substring(timeString.length - 2));
 			return (hours * 60 * 60) + (minutes * 60);
 		}
+
+		// Anytime, anyday
+		// ?all_day=1&all_week=1
+
+		// Morning, anyday
+		// ?all_day=0&morning=1&all_week=1
+
+		// Morning on Monday
+		// ?all_day=0&morning=1&all_week=0&monday=1
+
+		// Morning on Monday and Tuesday
+		// ?all_day=0&morning=1&all_week=0&monday=1&tuesday=1
+
 		isOpenNow = function(data, startTime, item, openDay) {
 			if (data.day && data.open && data.close) {
-        let nowSeconds;
-        let now;
-        const today = new Date();
-        now = !startTime ? new Date() : new Date(`October 25, 2017 ${startTime}`);
+				let nowSeconds;
+				let now;
+				const today = new Date();
+				now = !startTime ? new Date() : new Date(`October 25, 2017 ${startTime}`);
 
-        let pacificTime = (now.toString().indexOf('(PDT)') >= 0) || (now.toString().indexOf('(PST)') >= 0);
-        nowSeconds = (now.getHours() * 60 * 60) + (now.getMinutes() * 60) + now.getSeconds(); 
-        if (openDay) {
-          if (openDay.toLowerCase() === data.day.toLowerCase() &&
-          nowSeconds > getSeconds(data.open) &&
-          nowSeconds < getSeconds(data.close) ) {
-            // console.log('data:', data);
-            console.log('openDay', openDay, data);
-            return true;
-          } 
-        } else if (
-          nowSeconds > getSeconds(data.open) &&
-          nowSeconds < getSeconds(data.close) ) {
-            // console.log('data:', data);
-            return true;  
-          }
-        }
+				let pacificTime = (now.toString().indexOf('(PDT)') >= 0) || (now.toString().indexOf('(PST)') >= 0);
+				nowSeconds = (now.getHours() * 60 * 60) + (now.getMinutes() * 60) + now.getSeconds(); 
+				if (openDay) {
+					if (openDay.toLowerCase() === data.day.toLowerCase() &&
+					nowSeconds > getSeconds(data.open) &&
+					nowSeconds < getSeconds(data.close) ) {
+						// console.log('data:', data);
+						console.log('openDay', openDay, data);
+						return true;
+					} 
+				} else if (
+					nowSeconds > getSeconds(data.open) &&
+					nowSeconds < getSeconds(data.close) ) {
+						// console.log('data:', data);
+						return true;  
+					}
+				}
 				// TBD: Should we show a special notice if it’s opening soon or closing soon?
 			}
 			return false;
